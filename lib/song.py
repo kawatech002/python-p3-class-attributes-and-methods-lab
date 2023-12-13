07f6a9a5233e272de5e490d9673adad4d637d8ef
kawatech002 <kawanjiku94@gmail.com>
2023-12-14 00:48:15 +0300

    complete assignment

--- lib/song.py
+++ lib/song.py
@@ -7,7 +7,7 @@ class Song:
     artist_count = {}
 
     def __init__(self, name, artist, genre):
-        # Instance attributes
+        #  attributes for instance 
         self.name = name
         self.artist = artist
         self.genre = genre
